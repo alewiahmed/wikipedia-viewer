99b1df0e3ec12a8e9663c24bdb53515f37dfd8a3
Alewi Ahmed <alewimail@gmail.com>
2018-05-02 18:03:21 +0300

    Added a link which opens random articles.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>empty content</div>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://en.wikipedia.org/wiki/Special:Random"
+        >
+          Click here for a random article
+        </a>
       </div>
     );
   }
