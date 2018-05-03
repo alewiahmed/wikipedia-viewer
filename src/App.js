e58e3faaab70b00ff4eabceaab64f286f3d5d8a6
Alewi Ahmed <alewimail@gmail.com>
2018-05-03 09:40:40 +0300

    Added an animated input field.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://en.wikipedia.org/wiki/Special:Random"
-        >
-          Click here for a random article
-        </a>
+        <div>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://en.wikipedia.org/wiki/Special:Random"
+          >
+            Click here for a random article
+          </a>
+        </div>
+        <div className="form-container">
+          <form className="inputbox">
+            <input required="required" />
+            <button className="del" type="reset" />
+          </form>
+        </div>
       </div>
     );
   }
