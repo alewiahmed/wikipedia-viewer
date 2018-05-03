22da5b46d9263edbcd57219a90dd6a6585a95787
Alewi Ahmed <alewimail@gmail.com>
2018-05-03 10:38:31 +0300

    Added appropriate event listeners.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,49 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    text: '',
+    search: false
+  };
+
+  inputChanged = e => {
+    this.setState({
+      text: e.target.value
+    });
+  };
+
+  submit = e => {
+    let { text } = this.state;
+    e.preventDefault();
+    if (text !== '') {
+      this.setState({
+        search: true
+      });
+    }
+  };
+
+  reset = () => {
+    this.setState({ search: false });
+  };
   render() {
+    let { search } = this.state;
+    let appClass = search ? ' justify-to-top' : '';
     return (
-      <div className="App">
-        <div>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://en.wikipedia.org/wiki/Special:Random"
-          >
-            Click here for a random article
-          </a>
-        </div>
+      <div className={'App' + appClass}>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://en.wikipedia.org/wiki/Special:Random"
+        >
+          Click here for a random article
+        </a>
         <div className="form-container">
-          <form className="inputbox">
+          <form
+            className="inputbox"
+            onReset={this.reset}
+            onSubmit={this.submit}
+            onChange={this.inputChanged}
+          >
             <input required="required" />
             <button className="del" type="reset" />
           </form>
