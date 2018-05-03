222e7bd964e89319461951591e917d0cc43b9615
Alewi Ahmed <alewimail@gmail.com>
2018-05-03 17:52:14 +0300

    removed fetchDataFake function.

--- src/App.js
+++ src/App.js
@@ -31,48 +31,6 @@ class App extends Component {
     }
   };
 
-  fetchDataFake = async text => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          response: {
-            batchcomplete: '',
-            query: {
-              searchinfo: {
-                totalhits: 2,
-                suggestion: 'alawi',
-                suggestionsnippet: '<em>alawi</em>'
-              },
-              search: [
-                {
-                  ns: 0,
-                  title: 'Alawi (disambiguation)',
-                  pageid: 27755921,
-                  size: 1478,
-                  wordcount: 189,
-                  snippet:
-                    'Alawi (Arabic: علوي‎), also <span class="searchmatch">Alewi</span>, Alevi, Alavi, Alawid, or Alawite (French: Alaouite), means &quot;of or related to Ali&quot;, the Prophet Muhammad\'s nephew. It',
-                  timestamp: '2018-02-17T08:43:55Z'
-                },
-                {
-                  ns: 0,
-                  title: '27 May 2013 Baghdad bombings',
-                  pageid: 39524639,
-                  size: 11225,
-                  wordcount: 1092,
-                  snippet:
-                    'in Abu Saida and Zab injured two civilians and a Sahwa member.   Kareem <span class="searchmatch">Alewi</span>, a member of the Iraqi Parliament representing the National Iraqi Alliance',
-                  timestamp: '2017-06-30T13:01:49Z'
-                }
-              ]
-            }
-          },
-          searchTerm: text
-        });
-      }, 3000);
-    });
-  };
-
   fetchData = async text => {
     const URL = `http://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&list=search&prop=extracts&srsearch=${text}`;
     try {
@@ -93,7 +51,6 @@ class App extends Component {
     let { text } = this.state;
     this.setState({ loading: true });
     let results = await this.fetchData(text);
-    // let results = await this.fetchDataFake(text);
     if (!this.state.loading || this.state.text !== results.searchTerm) return;
     this.setState({ loading: false });
     if (!results.response.query.search.length) {
